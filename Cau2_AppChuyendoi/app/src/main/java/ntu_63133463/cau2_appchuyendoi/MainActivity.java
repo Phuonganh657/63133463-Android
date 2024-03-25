938f9e7cac23d29acbcff7e799dc6c54b48fd389
Trần Ngọc Phương Anh <146166561+Phuonganh657@users.noreply.github.com>
2024-03-25 19:00:51 +0700

    cau2_appchuyendoi

---
+++ Cau2_AppChuyendoi/app/src/main/java/ntu_63133463/cau2_appchuyendoi/MainActivity.java
@@ -0,0 +1,59 @@
+package ntu_63133463.cau2_appchuyendoi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    //Khai bao bien giao dien
+    EditText edtF, edtC;
+    Button btnCF, btnFC, btnclear;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtC = findViewById(R.id.edtC);
+        edtF = findViewById(R.id.edtF);
+        btnCF = findViewById(R.id.btnCF);
+        btnFC = findViewById(R.id.btnFC);
+        btnclear = findViewById(R.id.btnclear);
+        //xu ly click
+        btnCF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                int C = Integer.parseInt(edtC.getText().toString());
+                Double F = C*1.8 + 32;
+                edtF.setText(F+"");
+
+            }
+
+        });
+        btnFC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                int F = Integer.parseInt(edtC.getText().toString());
+                Double C = (F-32)/1.8;
+                edtC.setText(C+"");
+
+            }
+
+        });
+        btnclear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                edtC.setText("");
+                edtF.setText("");
+
+            }
+
+        });
+
+    }
+}
