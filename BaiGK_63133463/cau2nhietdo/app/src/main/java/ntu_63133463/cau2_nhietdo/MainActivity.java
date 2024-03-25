ffff244313f196e884c99d9aeab11f9b6f27fa14
Trần Ngọc Phương Anh <146166561+Phuonganh657@users.noreply.github.com>
2024-03-25 23:06:18 +0700

    2

--- BaiGK_63133463/cau2nhietdo/app/src/main/java/ntu_63133463/cau2_nhietdo/MainActivity.java
+++ BaiGK_63133463/cau2nhietdo/app/src/main/java/ntu_63133463/cau2_nhietdo/MainActivity.java
@@ -3,8 +3,14 @@ package ntu_63133463.cau2_nhietdo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    //Khai bao bien giao dien
+    EditText edtF, edtC;
+    Button btnCF, btnFC, btnclear;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
